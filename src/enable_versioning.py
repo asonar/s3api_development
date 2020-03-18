a8de0ddb1a1b854ecd15095c6bf07b604f3c2120
asonar <anirudha.sonar@gmail.com>
2020-03-17 17:55:25 -0700

    Add files via upload
    Examples : Enable bucket versioning and upload versions.

---
+++ src/enable_versioning.py
@@ -0,0 +1,33 @@
+import boto3
+session = boto3.session.Session()
+
+#Endpoint information
+endpoint_url = "http://Objects.scalcia.com"
+access_key = "bVSjIccNo738F5VmpaYjA4BTV_J7h_YH"
+secret_key = "_rCf_LXRPea1j9xmuCWKlFsWqu7Qa_1n"
+
+#Bucket name
+bucket = "bucket-from-api"
+
+#Connect to your Objects endpoint.
+s3c = session.client(aws_access_key_id=access_key,
+                           aws_secret_access_key=secret_key,
+                           endpoint_url=endpoint_url,
+                           service_name="s3")
+
+
+#Check if bucket exists and create it.
+print "Creating bucket : %s"%(bucket)
+try:
+  s3c.head_bucket(Bucket=bucket)
+except Exception as err:
+  s3c.create_bucket(Bucket=bucket)
+
+#Enable versioning
+print "Enabling versioning on bucket : %s"%(bucket)
+versioning_config = {"Status":"Enabled"}
+s3c.put_bucket_versioning(Bucket=bucket, VersioningConfiguration=versioning_config)
+
+#Get versioning status of  Bucket
+print "Versioning status of bucket : %s"%(bucket)
+print s3c.get_bucket_versioning(Bucket=bucket)
